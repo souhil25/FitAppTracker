fc4790f85b0da97e9adca95198777bb1e40bb0f8
Souhil <louddadsouhilabdelmalek@gmail.com>
2026-04-24 21:12:05 +0500

    FAT

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/FitAppTracker/",
+  base: "/",
   plugins: [react(), tailwindcss()]
 })
